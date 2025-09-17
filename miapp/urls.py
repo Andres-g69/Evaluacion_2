afa3b6c60dc2144f3454989ea51cb3619368c521
alumnos nunoa <alumnosnunoa@inacap.cl>
2025-09-17 09:47:49 -0300

    ventanas nuevas

--- miapp/urls.py
+++ miapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('publicacion', views.publicacion, name='publicacion'),
+    path('ranking', views.ranking, name='ranking'),
 ]
--- miapp/views.py
+++ miapp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
-def fort(request):
-    return render(request, 'fort.html')
+def publicacion(request):
+    return render(request, 'publicacion.html')
+def ranking(request):
+    return render(request, 'ranking.html')
